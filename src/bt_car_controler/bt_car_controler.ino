70b9a57425f7e47841d206d48252a8cc38810e3a
Rafael <akafael@users.noreply.github.com>
2021-09-05 14:12:28 -0300

    Code refactor

--- src/bt_car_controler/bt_car_controler.ino
+++ src/bt_car_controler/bt_car_controler.ino
@@ -5,9 +5,14 @@
              ponte H, através do protocolo serial
 */
 
+// Libraries ------------------------------------------------------------------
+
 #include <elapsedMillis.h>
 
+// Constants ------------------------------------------------------------------
+
 #define PINLED 13
+#define PIN_BUZZER 12
 
 #define PIN_MOTOR_BACK_1 9
 #define PIN_MOTOR_BACK_2 6
@@ -17,30 +22,42 @@
 
 #define PWD_SIGNAL_OFF 255
 
+#define MAX_SPEED 100
+
+elapsedMillis timerGlobal;
 elapsedMillis timerLoop;
 const int timeStep = 100; // ms
 
-elapsedMillis timerGlobal;
-
-int speedMotor;
-int speedStep;
-
-unsigned char speedTurn = 100;
-
-enum car_direction : char
+enum CarDirection : char
 {
   DIRECTION_RIGHT = 'R',
   DIRECTION_LEFT = 'L',
   DIRECTION_FRONT = 'F'
 };
 
+struct Car
+{
+  int Speed;
+  int Acceleration;
+  CarDirection Direction;
+};
+
+int speedMotor;
+int speedAcc;
+char carDirection;
+
+unsigned char speedTurn = 100;
+
 void setup() {
   
   Serial.begin(9600);
 
   pinMode(PINLED, OUTPUT);
+  pinMode(PIN_BUZZER, OUTPUT);
   pinMode(PIN_MOTOR_BACK_1, OUTPUT);
   pinMode(PIN_MOTOR_BACK_2, OUTPUT);
+  pinMode(PIN_MOTOR_FRONT_1, OUTPUT);
+  pinMode(PIN_MOTOR_FRONT_2, OUTPUT);
 
   analogWrite(PIN_MOTOR_BACK_1, PWD_SIGNAL_OFF);
   analogWrite(PIN_MOTOR_BACK_2, PWD_SIGNAL_OFF);
@@ -49,7 +66,8 @@ void setup() {
   analogWrite(PIN_MOTOR_FRONT_2, PWD_SIGNAL_OFF);
 
   // Speed
-  speedStep = -1;
+  speedAcc = -1;
+  carDirection = DIRECTION_FRONT;
 
   // Reset Timers
   timerGlobal = 0;
@@ -58,40 +76,36 @@ void setup() {
 void loop() {
   if( timerLoop >= timeStep ) // Control Frequency time
   {
-    // Reset Timer
-    timerLoop = 0; 
-
-    speedStep = (speedMotor >= 255)?-1:(speedMotor < -255) ?1:speedStep;
-    speedMotor += speedStep;
+    timerLoop = 0; // Reset Timer
     
-    setCarSpeed( speedMotor );
+    setCarSpeed( 0 );
+    setCarDirection( DIRECTION_RIGHT, 0 );
 
-    // 
-    Serial.print( timerGlobal );
-    Serial.print(",");
-    Serial.println( speedMotor );
+    printCarInfo();
   }
 }
+
+// Funções Auxiliares ----------------------------------------------
 
 /**
  * Define direção
  */
-void setCarDirection( const char carDirection )
+void setCarDirection( const char direction, int speedTurn )
 {
-  if( carDirection == DIRECTION_RIGHT )
+  if( direction == DIRECTION_RIGHT )
   {
-    analogWrite(PIN_MOTOR_FRONT_1, speedTurn);
-    analogWrite(PIN_MOTOR_FRONT_2, PWD_SIGNAL_OFF);
+    analogWrite(PIN_MOTOR_FRONT_1, -speedTurn);
+    analogWrite(PIN_MOTOR_FRONT_2, 0);
   }
-  else if( carDirection == DIRECTION_LEFT )
+  else if( direction == DIRECTION_LEFT )
   {
-    analogWrite(PIN_MOTOR_FRONT_1, PWD_SIGNAL_OFF);
-    analogWrite(PIN_MOTOR_FRONT_2, speedTurn);
+    analogWrite(PIN_MOTOR_FRONT_1, 0);
+    analogWrite(PIN_MOTOR_FRONT_2, -speedTurn);
   }
   else
   {
-    analogWrite(PIN_MOTOR_FRONT_1, PWD_SIGNAL_OFF);
-    analogWrite(PIN_MOTOR_FRONT_2, PWD_SIGNAL_OFF);
+    analogWrite(PIN_MOTOR_FRONT_1, 0);
+    analogWrite(PIN_MOTOR_FRONT_2, 0);
   }
 }
 
@@ -117,4 +131,115 @@ void setCarSpeed( const int speed )
     analogWrite(PIN_MOTOR_BACK_1, PWD_SIGNAL_OFF);
     analogWrite(PIN_MOTOR_BACK_2, PWD_SIGNAL_OFF);
   }
+}
+
+/**
+ * Define Velocidade e Direção a partir do comando recebido
+ */
+Car driverCommand( const char command )
+{
+  Car carCommand;
+  
+  switch( command )
+  {
+    case 'F': // Move Front
+    {
+      carCommand.Direction = DIRECTION_FRONT;
+      carCommand.Speed = MAX_SPEED;
+      carCommand.Acceleration = speedAcc;
+    }
+    break;
+
+    case 'G': // Move Front Left
+    {
+      carCommand.Direction = DIRECTION_LEFT;
+      carCommand.Speed = MAX_SPEED;
+      carCommand.Acceleration = speedAcc;
+    }
+    break;
+
+    case 'L': // Move Left
+    {
+      carCommand.Direction = DIRECTION_LEFT;
+      carCommand.Speed = 0;
+      carCommand.Acceleration = speedAcc;
+    }
+    break;
+
+    case 'I': // Move Front Right
+    {
+      carCommand.Direction = DIRECTION_RIGHT;
+      carCommand.Speed = MAX_SPEED;
+      carCommand.Acceleration = speedAcc;
+    }
+    break;
+
+    case 'R': // Move Right
+    {
+      carCommand.Direction = DIRECTION_RIGHT;
+      carCommand.Speed = 0;
+      carCommand.Acceleration = speedAcc;
+    }
+    break;
+
+    case 'H': // Move Backwards Left
+    {
+      carCommand.Direction = DIRECTION_LEFT;
+      carCommand.Speed = -MAX_SPEED;
+      carCommand.Acceleration = speedAcc;
+    }
+    break;
+
+    case 'J': // Move Backwards Right
+    {
+      carCommand.Direction = DIRECTION_RIGHT;
+      carCommand.Speed = -MAX_SPEED;
+      carCommand.Acceleration = speedAcc;
+    }
+    break;
+
+    default:
+    {
+      carCommand.Direction = DIRECTION_FRONT;
+      carCommand.Speed = 0;
+      carCommand.Acceleration = 0;
+    }
+  }
+
+  return carCommand;
+}
+
+void printCarInfo()
+{
+  Serial.print( millis() );
+  Serial.print(",");
+  Serial.print( timerGlobal );
+  Serial.print(",");
+  Serial.print( carDirection );
+  Serial.print(",");
+  Serial.println( speedMotor );
+}
+
+void printInfo( Car carState )
+{
+  Serial.print( millis() );
+  Serial.print(",");
+  Serial.print( carState.Direction );
+  Serial.print(",");
+  Serial.print( carState.Speed );
+  Serial.print(",");
+  Serial.println( carState.Acceleration );
+}
+
+// Funções para Testes em Geral ----------------------------------------------
+
+void testCarSpeed()
+{
+  const int maxSpeed = 255;
+
+  // Set Acceleration
+  speedAcc = (speedMotor >= maxSpeed)?-1:(speedMotor < -maxSpeed) ?1:speedAcc;
+  speedMotor += speedAcc;
+
+  setCarSpeed( speedMotor );
 }
